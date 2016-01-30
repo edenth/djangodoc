3ad64981ef3d71abe50119b96dacfac057fe7441
nut <parin.nut@gmail.com>
2016-01-31 01:23:43 +0700

    add IndexView

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,8 +4,28 @@ from .models import Question
 from .models import Choice
 from django.template import loader
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
